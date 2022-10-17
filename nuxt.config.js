19bbbde60b056d837ec539f9758305ec7ebb817e
ohtsubomber <ohtsubomber@gmail.com>
2022-10-17 18:56:49 +0900

    baseURLの修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: 'github-pages'
+    base: '/github-pages/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
